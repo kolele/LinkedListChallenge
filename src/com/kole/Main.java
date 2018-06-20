5e86e462cd5132e203fad81a1c983e2f4fd3b694
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-20 17:40:57 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -1,8 +1,34 @@
 package com.kole;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+
+        printList(placesToVisit);
+
+        placesToVisit.add(1, "Alice Springs");
+        printList(placesToVisit);
+
+        placesToVisit.remove(4);
+        printList(placesToVisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList){
+        Iterator<String> i = linkedList.iterator();
+        while (i.hasNext()){
+            System.out.println("Now visiting " + i.next());
+        }
+        System.out.println("===================================");
     }
 }

2470e3bef30c4919495510ad856ac0c9fbb35db1
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-20 19:21:39 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -1,8 +1,14 @@
 package com.kole;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        ArrayList<Album> albums = new ArrayList<Album>();
+        LinkedList<Song> playList = new LinkedList<Song>();
+        playList.add(new Song("Kole", 12));
+
     }
 }
---
+++ src/com/kole/Song.java
@@ -0,0 +1,19 @@
+package com.kole;
+
+public class Song {
+    private String name;
+    private int duration;
+
+    public Song(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}

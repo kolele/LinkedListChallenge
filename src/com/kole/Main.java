956af8481b334c65e140f70257c80b220119a9c1
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-24 15:29:43 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -2,15 +2,35 @@ package com.kole;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Album> albums = new ArrayList<Album>();
+        //ArrayList<Album> albums = new ArrayList<Album>();
         LinkedList<Song> playList = new LinkedList<Song>();
         Album rideTheLightning = new Album("Ride the Lightning");
         rideTheLightning.addSongToAlbum("Whiplash", 12.55);
-        rideTheLightning.addToPlaylist("Whiplash", playList);
+        rideTheLightning.addSongToAlbum("One", 12.55);
+        rideTheLightning.addSongToAlbum("And Justice for All!", 12.55);
+        rideTheLightning.addSongToAlbum("Fuel", 12.55);
+        rideTheLightning.addSongToAlbum("Some Kind of Monster", 12.55);
+
+        rideTheLightning.addToPlaylist("Fuel", playList);
+        rideTheLightning.addToPlaylist("One", playList);
+        rideTheLightning.addToPlaylist("Some Kind of Monster", playList);
+        rideTheLightning.addToPlaylist("And Justice for All!", playList);
+
+        play(playList);
+
+    }
+
+    public static void play(LinkedList<Song> playList){
+        ListIterator<Song> listIterator = playList.listIterator();
+        while (listIterator.hasNext()){
+            System.out.println("Now playing " + listIterator.hasNext());
+        }
+
 
     }
 
